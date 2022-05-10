8b14c254597a168d77c8afc5094f09d0434e14c4
Sahil Garg <sahil.garg891@gmail.com>
2022-05-10 12:39:00 +0530

    Update

--- VHDL/code_mem.vhdl
+++ VHDL/code_mem.vhdl
@@ -4,7 +4,7 @@ use ieee.numeric_std.all;
 
 entity code_mem is
 port(
-    pc: in std_logic_vector(15 downto 0);
+    address_in: in std_logic_vector(15 downto 0);
     clk: in std_logic;
     d_out: out std_logic_vector(15 downto 0)
 );
@@ -16,10 +16,10 @@ architecture arch of code_mem is
     signal data_temp: mem_block := ("0001101100011000","1100101011010011","0011000001010000", others => (others =>'0'));
 begin
  
-    add_in <= pc;
+    add_in <= address_in;
     d_out <= data_temp(to_integer(unsigned(add_in(7 downto 0))));
 
-  end arch;
+end arch;
 
 
 
--- VHDL/data_mem.vhdl
+++ VHDL/data_mem.vhdl
@@ -4,15 +4,16 @@ use ieee.numeric_std.all;
 
 entity data_mem is
 port(
-    alu_or_eq, rf_d1, ir: in std_logic_vector(15 downto 0);
+    mem_a, mem_din: in std_logic_vector(15 downto 0);
     clk: in std_logic;
-    mem_cw: in std_logic_vector(3 downto 0);
+    opcode: in std_logic_vector(3 downto 0);
     d_out: out std_logic_vector(15 downto 0)
 );
 end entity data_mem;
 
 architecture arch of data_mem is
     signal add_in, d_in: std_logic_vector(15 downto 0);
+    signal wb_en: std_logic;
     type mem_block is array (0 to 255) of std_logic_vector (15 downto 0); --512 Byte memory
     signal data_temp: mem_block := ("0001101100011000","1100101011010011","0011000001010000", others => (others =>'0'));
 begin
@@ -21,25 +22,18 @@ begin
     
     begin
 
-        if mem_cw(2 downto 1)="01" then
-              add_in<= d1;	  
-		  elsif mem_cw(2 downto 1)="10" then
-              add_in<= t1;    
-        elsif mem_cw(2 downto 1)="11" then
-              add_in<= t3;
-        end if; 
+        if opcode = "0111" then --LW
+            add_in<= mem_a;
+            wb_en = '0';
+        elsif opcode = "0101" then --SW
+            add_in<= mem_a;
+            d_in <= mem_din;
+            wb_en = '1';
+        end if;
         
-           
-          case mem_cw(3) is
-            when '1' =>
-              d_in <= d1;
-            when others =>
-              d_in <= t1;
-          end case;
-        
-
+        -- write_back
         if(clk'event and clk = '0') then
-          if (mem_cw(0)='1') then
+          if (wb_en='1') then
             data_temp(to_integer(unsigned(add_in(7 downto 0)))) <= d_in;
           end if;
         end if;
